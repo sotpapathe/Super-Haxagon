caa716d63e10bf35f8b93a846f7f659f34a22ed2
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2025-10-15 22:40:09 +0300

    [WIP] Get rid of pspaudiolib
    TODO: check how other PSP games do audio playback
    TODO: fix crash when switching music, fix cracking?

--- driver/PSP/source/PlatformPSP.cpp
+++ driver/PSP/source/PlatformPSP.cpp
@@ -11,12 +11,14 @@
 #include "Driver/Tools/Random.hpp"
 
 #include <filesystem>
+#include <pspaudio.h>
 #include <pspaudiolib.h>
 #include <psploadexec.h>
 #include <psprtc.h>
 #include <pspthreadman.h>
 #include <psputils.h>
 
+#include "CommonPSP.hpp"
 #include "ControlsPSP.hpp"
 
 // The PSP CPU is little endian and the sizes of basic types are the following:
@@ -48,8 +50,8 @@ namespace SuperHaxagon {
 			running = initCallbacks();
 			screen = createScreen(debug);
 			initControls();
-			if (pspAudioInit() < 0) {
-				message(Dbg::FATAL, "platform", "error initializing audio");
+			if (sceAudioChReserve(PSP_MUSIC_CHANNEL, PSP_NUM_AUDIO_SAMPLES, PSP_AUDIO_FORMAT_STEREO) < 0) {
+				message(Dbg::FATAL, "platform", "error reserving music channel");
 				shutdown();
 			}
 
@@ -57,7 +59,7 @@ namespace SuperHaxagon {
 		}
 
 		void shutdown() {
-			pspAudioEnd();
+			sceAudioChRelease(PSP_MUSIC_CHANNEL);
 			sceKernelExitGame();
 			debugStream.close();
 		}
